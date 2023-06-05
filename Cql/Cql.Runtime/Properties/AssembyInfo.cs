f82660c547b19ec2e93ef00b4f41f88eb9b4dda3
Marten Smits <m.smits@furore.com>
2023-06-05 14:45:41 +0200

    added InternalsVisibleTo to AssemblyInfo

---
+++ Cql/Cql.Runtime/Properties/AssembyInfo.cs
@@ -0,0 +1,8 @@
+﻿
+
+#if DEBUG
+[assembly: System.Runtime.CompilerServices.InternalsVisibleToAttribute("CoreTests")]
+#endif
+#if RELEASE
+[assembly: System.Runtime.CompilerServices.InternalsVisibleToAttribute("CoreTests, PublicKey=00240000048000009400000006020000002400005253413100040000010001006d866cf0363db3976082a2439c420bd942b78243c0ce6c289f3c07d6901ecf149c38bafc12167f6eba7618b288e5327a7e4ab554c9a294ae6c68d7455b6562f846455b6bfdde68542a7046a632f1730a1647f2d365f8a25f73860c14d1428e7319df673e2d3212fa535f310c52a7c7ec17de772cb49961f4ea195fa4ea601dcd")]
+#endif
