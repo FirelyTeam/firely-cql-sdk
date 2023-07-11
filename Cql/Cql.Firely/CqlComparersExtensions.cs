05573235240e413f38f908ccfd122544237d078f
Evan Machusak <machusak@ncqa.org>
2023-07-11 15:06:17 -0400

    EOB test pass 100%

--- Cql/Cql.Firely/CqlComparersExtensions.cs
+++ Cql/Cql.Firely/CqlComparersExtensions.cs
@@ -13,10 +13,26 @@ namespace Hl7.Cql.Firely
     {
         public static CqlComparers AddFirelyComparers(this CqlComparers comparers)
         {
+            comparers.Register(typeof(Canonical), new IValueComparer<string?>());
+            comparers.Register(typeof(Code), new IValueComparer<string?>());
+            comparers.Register(typeof(Date), new IValueComparer<string?>());
+            comparers.Register(typeof(FhirBoolean), new IValueComparer<bool?>());
+            comparers.Register(typeof(FhirDateTime), new IValueComparer<string?>());
             comparers.Register(typeof(FhirDecimal), new IValueComparer<decimal?>());
+            comparers.Register(typeof(FhirString), new IValueComparer<string?>());
+            comparers.Register(typeof(FhirUri), new IValueComparer<string?>());
+            comparers.Register(typeof(FhirUrl), new IValueComparer<string?>());
             comparers.Register(typeof(Id), new IValueComparer<string>());
-            comparers.Register(typeof(Identifier), new IValueComparer<int?>());
             comparers.Register(typeof(Integer), new IValueComparer<int?>());
+            comparers.Register(typeof(Instant), new IValueComparer<DateTimeOffset?>());
+            comparers.Register(typeof(Markdown), new IValueComparer<string?>());
+            comparers.Register(typeof(Oid), new IValueComparer<string?>());
+            comparers.Register(typeof(PositiveInt), new IValueComparer<int?>());
+            comparers.Register(typeof(Time), new IValueComparer<string?>());
+            comparers.Register(typeof(UnsignedInt), new IValueComparer<int?>());
+            comparers.Register(typeof(Uuid), new IValueComparer<string?>());
+
+            comparers.Register(typeof(Identifier), new IdentifierComparer(comparers, comparers));
             return comparers;
         }
     }
