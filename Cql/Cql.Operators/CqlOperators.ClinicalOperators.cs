8b92cd999d4524f05a36423e5ebb400af702e943
Marco Visser <marco@fire.ly>
2023-09-11 10:47:01 +0200

    Use the correct GitHub repo name

--- Cql/Cql.Operators/CqlOperators.ClinicalOperators.cs
+++ Cql/Cql.Operators/CqlOperators.ClinicalOperators.cs
@@ -4,7 +4,7 @@
  * See the file CONTRIBUTORS for details.
  * 
  * This file is licensed under the BSD 3-Clause license
- * available at https://raw.githubusercontent.com/FirelyTeam/cql-sdk/main/LICENSE
+ * available at https://raw.githubusercontent.com/FirelyTeam/firely-cql-sdk/main/LICENSE
  */
 
 using Hl7.Cql.Operators;
