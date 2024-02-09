cc439decf1fe0245cbfdb8eeadb789098e1dffb5
Paul den Boer <denboer.paul@gmail.com>
2024-02-09 10:02:33 +0100

    Add ReflectiionUtilitie

---
+++ Cql/Cql.Compiler/ConstructorInfos.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Hl7.Cql.Abstractions;
+using Hl7.Cql.Compiler.Infrastructure;
+using Hl7.Cql.Primitives;
+
+namespace Hl7.Cql.Compiler;
+
+internal static class ConstructorInfos
+{
+    public static ConstructorInfo CqlCode { get; } = 
+        ReflectionUtility.ConstructorOf(() => new CqlCode(default(string?), default(string?), default(string?), default(string?)))!;
+
+    public static ConstructorInfo CqlConcept { get; } =
+        ReflectionUtility.ConstructorOf(() => new CqlConcept(default(IEnumerable<CqlCode>)!, default(string?)))!;
+    
+    public static ConstructorInfo CqlDeclarationAttribute { get; } =
+        ReflectionUtility.ConstructorOf(() => new CqlDeclarationAttribute(default(string)!))!;
+
+    public static ConstructorInfo LazyOfBoolCtor { get; } =
+        ReflectionUtility.ConstructorOf(() => new Lazy<bool?>(default(Func<bool?>)!))!;
+
+    public static ConstructorInfo NotImplementedException { get; } =
+        ReflectionUtility.ConstructorOf(() => new NotImplementedException(default(string?)))!;
+
+    public static ConstructorInfo CqlValueSet { get; } =
+        ReflectionUtility.ConstructorOf(() => new CqlValueSet(default(string?)!, default(string?)!))!;
+
+    public static ConstructorInfo CqlRatio { get; } =
+        ReflectionUtility.ConstructorOf(() => new CqlRatio(default(CqlQuantity?), default(CqlQuantity?)))!;
+
+    public static ConstructorInfo CqlQuantity { get; } = 
+        ReflectionUtility.ConstructorOf(() => new CqlQuantity(default(decimal?), default(string?)))!;
+
+    public static ConstructorInfo ListOf<T>() =>
+        ReflectionUtility.ConstructorOf(() => new List<T>((IEnumerable<T>)default(IEnumerable<T>)!))!;
+
+    public static ConstructorInfo ListOf(Type elementType) =>
+        (ConstructorInfo)
+            // Call ListOf<T> via reflection
+            ReflectionUtility.MethodOf(() => ListOf<object>())!
+            .GetGenericMethodDefinition()
+            .MakeGenericMethod(elementType)
+            .Invoke(null, null)!;
+}
---
+++ Cql/Cql.Compiler/Infrastructure/ReflectionUtiltity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace Hl7.Cql.Compiler.Infrastructure;
+
+internal static class ReflectionUtility
+{
+    public static ConstructorInfo? ConstructorOf<T>(Expression<Func<T>> fnToCtor)
+        => fnToCtor.Body is NewExpression ne
+            ? ne.Constructor
+            : null;
+
+    public static MethodInfo? MethodOf(Expression<Action> fnToMethodCall)
+        => fnToMethodCall.Body is MethodCallExpression mce
+            ? mce.Method
+            : null;
+
+}
