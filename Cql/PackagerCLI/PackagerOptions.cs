0cdde33201faf0782f012e63e79040ebba601a71
Paul den Boer <denboer.paul@gmail.com>
2024-02-14 22:54:33 +0100

    Begin with host building

---
+++ Cql/PackagerCLI/PackagerOptions.cs
@@ -0,0 +1,53 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.Extensions.Options;
+
+namespace Hl7.Cql.Packager;
+
+internal class PackagerOptions : IValidateOptions<PackagerOptions>
+{
+    public const string ConfigSection = "Packager";
+
+    public static IDictionary<string, string> BuildSwitchMappings() => new SortedDictionary<string, string>
+    {
+        ["--elm"] = ConfigSection + ':' + nameof(ElmDirectory),
+        ["--cql"] = ConfigSection + ':' + nameof(CqlDirectory),
+        ["--fhir"] = ConfigSection + ':' + nameof(FhirDirectory),
+        ["--cs"] = ConfigSection + ':' + nameof(CSharpDirectory),
+        ["--d"] = ConfigSection + ':' + nameof(Debug),
+        ["--f"] = ConfigSection + ':' + nameof(Force),
+        ["--canonical-root-url"] = ConfigSection + ':' + nameof(CanonicalRootUrl),
+    };
+
+    [Required(AllowEmptyStrings = false)]
+    public string ElmDirectory { get; set; } = null!;
+
+    [Required(AllowEmptyStrings = false)]
+    public string CqlDirectory { get; set; } = null!;
+
+    public string? CSharpDirectory { get; set; }
+
+    public string? FhirDirectory { get; set; }
+
+    public bool Debug { get; set; }
+
+    public bool Force { get; set; }
+
+    public string? CanonicalRootUrl { get; set; }
+
+    public ValidateOptionsResult Validate(string? name, PackagerOptions options)
+    {
+        ValidateOptionsResultBuilder b = new();
+
+        if (string.IsNullOrWhiteSpace(options.ElmDirectory))
+        {
+            b.AddError("The ELM directory is required.", "--elm");
+        }
+
+        if (string.IsNullOrWhiteSpace(options.CqlDirectory))
+        {
+            b.AddError("The CQL directory is required.", "--cql");
+        }
+
+        return b.Build();
+    }
+}
--- Cql/PackagerCLI/Program.cs
+++ Cql/PackagerCLI/Program.cs
@@ -1,109 +1,205 @@
 ﻿#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
-using Hl7.Cql.Packaging;
-using Hl7.Cql.Packaging.ResourceWriters;
+using System.Diagnostics;
+using System.Globalization;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using Serilog;
 
 namespace Hl7.Cql.Packager
 {
+
     public static class Program
     {
         public static int Main(string[] args)
         {
+            args = args.Except(new[] { "--elm" }).ToArray();
+
+            if (args.Length == 0 ||
+                new[] { "-?", "-h", "-help" }.Any(s => args.Contains(s, StringComparer.InvariantCultureIgnoreCase)))
+                return ShowHelp();
+
+            var hostBuilder = Host.CreateDefaultBuilder()
+                .ConfigureAppConfiguration(ConfigureConfiguration)
+                .ConfigureLogging(ConfigureLogging)
+                .ConfigureServices(ConfigureServices);
+
+            using var host = hostBuilder.Build();
+            using var mainScope = host.Services.CreateScope();
+            var packageService = mainScope.ServiceProvider.GetRequiredService<PackagerService>();
+            return packageService.Run();
+
+            void ConfigureConfiguration(HostBuilderContext hostContext, IConfigurationBuilder config)
+            {
+                IDictionary<string, string> switchMappings = PackagerOptions.BuildSwitchMappings();
+                config.AddCommandLine(args, switchMappings);
+            }
+
+            static void ConfigureLogging(HostBuilderContext hostContext, ILoggingBuilder logging)
+            {
+                logging.AddFilter(level => level >= LogLevel.Trace);
+                logging.AddConsole(console =>
+                {
+                    console.LogToStandardErrorThreshold = LogLevel.Error;
+                });
+                var logFile = Path.Combine(".", "build.txt");
+                Log.Logger = new LoggerConfiguration()
+                    .Enrich.FromLogContext()
+                    .WriteTo
+                    .File(logFile, formatProvider: CultureInfo.InvariantCulture)
+                    .CreateLogger();
+                logging.AddSerilog();
+            }
+            
+            static void ConfigureServices(HostBuilderContext hostContext, IServiceCollection services)
+            {
+                services.AddTransient<PackagerService>();
+
+                services.AddOptions<PackagerOptions>()
+                    .BindConfiguration(PackagerOptions.ConfigSection, o => o.ErrorOnUnknownConfiguration = true)
+                    .ValidateOnStart();
+                services.AddSingleton<IValidateOptions<PackagerOptions>, PackagerOptions>();
+            }
+        }
+
+        private static int ShowHelp()
+        {
+            Console.WriteLine(
+                """
+                Packager CLI Usage:
+                
+                    -?|-h|-help                     Show this help
+                
+                    --elm       <directory>         Library root directory
+                    --cql       <directory>         CQL root directory
+                    [--fhir]    <file|directory>    Resource location, either file name or directory
+                    [--cs]      <file|directory>    C# output location, either file name or directory
+                    [--d]       <true|false>        Produce as a debug assembly
+                    [--f]       <true|false>        Force an overwrite even if the output file already exists
+                    [--canonical-root-url] <url>    The root url used for the resource canonical.
+                                                    If omitted a '#' will be used
+                    [--override-utc-date-time] <utc-date-time>
+                                                    The date time to use for the library and resource last updated.
+                                                    (example: 2000-12-31T23:59:59.99Z)
+                                                    If omitted the current date time will be used.
+                """);
+            return -1;
+        }
+    }
+
+    internal class PackagerService
+    {
+        private readonly PackagerOptions _packagerOptions;
+
+        public PackagerService(IOptions<PackagerOptions> packageArgsOptions)
+        {
+            _packagerOptions = packageArgsOptions.Value;
+        }
+
+        public int Run()
+        {
+            Console.WriteLine("Hello World!");
+            return 0;
+        }
+    }
+
+    /*
+    public static class Program
+    {
+        public static int Main(string[] args)
+        {
+            if (args.Length == 0 ||
+                new[] { "-?", "-h", "-help" }.Any(s => args.Contains(s, StringComparer.InvariantCultureIgnoreCase)))
+                return ShowHelp();
+
+            var switchMappings = new SortedDictionary<string, string>
+            {
+                ["--elm"] = nameof(CommandlineArgs.ElmDirectory),
+                ["--cql"] = nameof(CommandlineArgs.CqlDirectory),
+                ["--fhir"] = nameof(CommandlineArgs.FhirDirectory),
+                ["--cs"] = nameof(CommandlineArgs.CSharpDirectory),
+                ["--d"] = nameof(CommandlineArgs.Debug),
+                ["--f"] = nameof(CommandlineArgs.Force),
+                ["--canonical-root-url"] = nameof(CommandlineArgs.CanonicalRootUrl),
+                ["--override-utc-date-time"] = nameof(CommandlineArgs.OverrideUtcDateTime)
+            };
+
             var config = new ConfigurationBuilder()
-                .AddCommandLine(args)
+                .AddCommandLine(args, switchMappings)
                 .Build();
 
-            if (args.Length == 0 || config["?"] != null || config["h"] != null || config["help"] != null)
-                return ShowHelp();
+            var configKeyValues = config.AsEnumerable();
 
-            if (config.AsEnumerable()
+            if (configKeyValues
                     .Select(kv => kv.Key)
-                    .Except(supportedArgs)
+                    .Except(switchMappings.Values)
                     .ToList() is { Count: > 0 } unknownArgs)
             {
-                Console.Error.WriteLine($"Unknown args: {string.Join(", ", unknownArgs)}.");
-                ShowHelp();
-                return -1;
+                return ShowErrorWithHelp($"Unknown args: {string.Join(", ", unknownArgs)}.");
             }
 
+            CommandlineArgs rawOptions = new CommandlineArgs();
+            config.Bind(rawOptions);
 
-            // elm
+            if (string.IsNullOrEmpty(rawOptions.ElmDirectory))
+                return ShowErrorWithHelp("--elm: required argument missing.");
 
-            if (config["elm"] is not {} elmArg)
-                return ShowHelp();
+            if (string.IsNullOrEmpty(rawOptions.CqlDirectory))
+                return ShowErrorWithHelp("--cql: required argument missing.");
 
-            var elmDir = new DirectoryInfo(elmArg);
-            if (!elmDir.Exists)
+            string lastArgName = "";
+            PackageOptions options = null!;
+            try
             {
-                Console.Error.WriteLine($"-elm: path {elmArg} does not exist.");
-                return -1;
-            }
-
-            // cql
-
-            if (config["cql"] is not {} cqlArg )
-                return ShowHelp();
-
-            var cqlDir = new DirectoryInfo(cqlArg);
-            if (!cqlDir.Exists)
-            {
-                Console.Error.WriteLine($"-cql: path {cqlArg} does not exist.");
-                return -1;
-            }
-
-            // d
-
-            if (config["d"] is {} dArg && !bool.TryParse(dArg, out bool debug))
-            {
-                Console.Error.WriteLine($"-d: expected true|false, got {dArg}");
-                return -1;
-            }
-
-            // cs
-
-            DirectoryInfo? csDir = null;
-            if (config["cs"] is {} csArg)
-            {
-                csDir = new DirectoryInfo(csArg);
-                if (!csDir.Exists)
+                options = new PackageOptions
                 {
-                    EnsureDirectory(csDir);
-                }
+                    ElmDirectory = rawOptions.ElmDirectory
+                        .Do(() => lastArgName = "--elm")
+                        .GetDirectory()
+                        .MustExist(),
+                    CqlDirectory = rawOptions.CqlDirectory
+                        .Do(() => lastArgName = "--cql")
+                        .GetDirectory()
+                        .MustExist(),
+                    CSharpDirectory = rawOptions.CSharpDirectory
+                        .Do(() => lastArgName = "--cs")
+                        .GetDirectory()
+                        .CreateIfNotExists(),
+                    FhirDirectory = rawOptions.FhirDirectory
+                        .Do(() => lastArgName = "--fhir")
+                        .GetDirectory()
+                        .CreateIfNotExists(),
+                    Debug = rawOptions.Debug
+                        .Do(() => lastArgName = "--d"),
+                    Force = rawOptions.Force
+                        .Do(() => lastArgName = "--f"),
+                    CanonicalRootUrl = rawOptions.CanonicalRootUrl
+                        .Do(() => lastArgName = "--canonical-root-url")
+                        .Return(s => s?.TrimEnd('/')),
+                    OverrideUtcDateTime = rawOptions.OverrideUtcDateTime
+                        .Do(() => lastArgName = "--override-utc-date-time")
+                        .GetUtcDateTime()
+                };
             }
-
-            // f
-
-            if (config["f"] is {} fArg 
-                && !bool.TryParse(fArg, out var force))
+            catch (ProcessOptionsException pe)
             {
-                Console.Error.WriteLine($"-f: expected true|false, got {fArg}");
-                return -1;
+                return ShowErrorWithHelp($"{lastArgName}: {pe.Message}");
             }
 
-            // fhir
-
-            DirectoryInfo? fhirDir = null;
-            if (config["fhir"] is {} fhirArg)
-            {
-                fhirDir = new DirectoryInfo(fhirArg);
-                if (!fhirDir.Exists)
-                {
-                    EnsureDirectory(fhirDir);
-                }
-            }
-
-            // canonical-root-url
-
-            var resourceCanonicalRootUrl = config["canonical-root-url"]?.TrimEnd('/');
-
-            Package(elmDir, cqlDir, csDir, fhirDir, resourceCanonicalRootUrl);
+            Package(options);
             return 0;
         }
 
-        private static void Package(DirectoryInfo elmDir, DirectoryInfo cqlDir, DirectoryInfo? csDir, DirectoryInfo? fhirDir, string? resourceCanonicalRootUrl)
+        private static void Package(PackageOptions options)
         {
+            var fhirDir = options.FhirDirectory;
+            var csDir = options.CSharpDirectory;
+            var elmDir = options.ElmDirectory;
+            var cqlDir = options.CqlDirectory;
+            var resourceCanonicalRootUrl = options.CanonicalRootUrl;
+
             var logLevel = LogLevel.Trace;
             var logFactory = LoggerFactory
                 .Create(logging =>
@@ -126,44 +222,98 @@ namespace Hl7.Cql.Packager
             var cliLogger = logFactory.CreateLogger("CLI");
 
             List<ResourceWriter> resourceWriters = new();
-            if (fhirDir != null) resourceWriters.Add(new FhirResourceWriter(fhirDir, cliLogger));
+            if (fhirDir != null) resourceWriters.Add(new FhirResourceWriter(fhirDir, cliLogger, options.OverrideUtcDateTime));
             if (csDir != null) resourceWriters.Add(new CSharpResourceWriter(csDir, cliLogger));
 
             var resourcePackager = new ResourcePackager(logFactory, resourceWriters.ToArray());
             resourcePackager.Package(new PackageArgs(elmDir, cqlDir, resourceCanonicalRootUrl: resourceCanonicalRootUrl));
         }
 
-
-        private static void EnsureDirectory(DirectoryInfo directory, int timeoutMs = 5000)
-        {
-            var now = DateTime.Now;
-            var loop = true;
-            var timeout = TimeSpan.FromMilliseconds(timeoutMs);
-            while (!directory.Exists && loop)
-            {
-                directory.Create();
-                directory.Refresh();
-                if (DateTime.Now.Subtract(now) > timeout)
-                    throw new InvalidOperationException($"Unable to create directory {directory.FullName} after {timeout}");
-            }
-        }
-
-        private static string[] supportedArgs = new[] { "elm", "cql", "fhir", "cs", "d", "f", "canonical-root-url" };
-
         private static int ShowHelp()
         {
-            Console.WriteLine();
-            Console.WriteLine("Packager CLI");
-            Console.WriteLine();
-            Console.WriteLine($"\t--elm <directory>\tLibrary root path");
-            Console.WriteLine($"\t--cql <directory>\tCQL root path");
-            Console.WriteLine($"\t[--fhir] <file>\tResource location, either file name or directory");
-            Console.WriteLine($"\t[--cs] <file>\tC# output location, either file name or directory");
-            Console.WriteLine($"\t[--d] true|false\t\tProduce as a debug assembly");
-            Console.WriteLine($"\t[--f] true|false\tIf output file already exists, overwrite");
-            Console.WriteLine($"\t[--canonical-root-url] <url>\tThe root url used for the resource canonical. If omitted a '#' will be used");
-            Console.WriteLine();
+            Console.WriteLine(
+                """
+                Packager CLI Usage:
+                
+                    -?|-h|-help                     Show this help
+                
+                    --elm       <directory>         Library root directory
+                    --cql       <directory>         CQL root directory
+                    [--fhir]    <file|directory>    Resource location, either file name or directory
+                    [--cs]      <file|directory>    C# output location, either file name or directory
+                    [--d]       <true|false>        Produce as a debug assembly
+                    [--f]       <true|false>        Force an overwrite even if the output file already exists
+                    [--canonical-root-url] <url>    The root url used for the resource canonical. 
+                                                    If omitted a '#' will be used
+                    [--override-utc-date-time] <utc-date-time>
+                                                    The date time to use for the library and resource last updated.
+                                                    (example: 2000-12-31T23:59:59.99Z) 
+                                                    If omitted the current date time will be used.
+                """);
             return -1;
+        }
+        private static int ShowErrorWithHelp(string error)
+        {
+            Console.Error.WriteLine(error);
+            return ShowHelp();
+        }
+
+    }
+    */
+
+    file static class Extensions
+    {
+        public static T Do<T>(this T value, Action action)
+        {
+            action();
+            return value;
+        }
+
+        public static TR Return<T, TR>(this T value, Func<T, TR> func) => 
+            func(value);
+
+        public static DirectoryInfo? GetDirectory(this string? dirText) => string.IsNullOrEmpty(dirText) ? null : new DirectoryInfo(Path.GetFullPath(dirText));
+
+        public static DateTime? GetUtcDateTime(this string? utcDateTimeText) => 
+            string.IsNullOrEmpty(utcDateTimeText) 
+                ? null 
+                : DateTime.TryParse(utcDateTimeText, CultureInfo.InvariantCulture, DateTimeStyles.AdjustToUniversal, out var utcDateTime)
+                ? utcDateTime
+                : throw new ProcessOptionsException("Not a Valid UTC Date Time. (example: 2000-12-31T23:59:59.99Z)");
+
+        public static DirectoryInfo MustExist(this DirectoryInfo? dir) =>
+            dir switch
+            {
+                { Exists: true } v => v,
+                { Exists: false } v => throw new ProcessOptionsException($"Directory '{v.FullName}' does not exist."),
+                _ => throw new ProcessOptionsException("Directory must be specified and exist.")
+            };
+
+        public static DirectoryInfo? CreateIfNotExists(this DirectoryInfo? dir) =>
+            dir switch
+            {
+                { Exists: false } v => CreateDirectoryWithTimeout(v),
+                _ => dir,
+            };
+
+        private static DirectoryInfo CreateDirectoryWithTimeout(DirectoryInfo d, int timeoutMs = 5000)
+        {
+            Stopwatch sw = Stopwatch.StartNew();
+            while (!d.Exists && sw.ElapsedMilliseconds < timeoutMs)
+            {
+                d.Create();
+                d.Refresh();
+            }
+            if (!d.Exists)
+                throw new ProcessOptionsException($"Unable to create directory {d.FullName} after {timeoutMs}ms");
+            return d;
+        }
+    }
+
+    file class ProcessOptionsException : Exception
+    {
+        public ProcessOptionsException(string message) : base(message)
+        {
         }
     }
 }
