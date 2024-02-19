90e7ae6a17da4ceeb0df442c56ce2d6a212e1c1d
Paul den Boer <denboer.paul@gmail.com>
2024-02-19 09:26:05 +0100

    Update usage for PackageCLI, add some code comments to loading of resourcewirters in dependency injection

--- Cql/PackagerCLI/Program.cs
+++ Cql/PackagerCLI/Program.cs
@@ -57,19 +57,19 @@ public class Program
         """
         Packager CLI Usage:
         
-            -?|-h|-help                     Show this help
-        
-            --elm       <directory>         Library root directory
-            --cql       <directory>         CQL root directory
-            [--fhir]    <file|directory>    Resource location, either file name or directory
-            [--cs]      <file|directory>    C# output location, either file name or directory
-            [--d]       <true|false>        Produce as a debug assembly
-            [--f]       <true|false>        Force an overwrite even if the output file already exists
-            [--canonical-root-url]          The root url used for the resource canonical.
-                        <url>               If omitted a '#' will be used
-            [--override-utc-date-time]      The date time to use for the library and resource last updated.
-                        <utc-date-time>     If omitted the current date time will be used.
-                                            (example: 2000-12-31T23:59:59.99Z)
+            -?|-h|-help                                Show this help
+                                                       
+            --elm                  <directory>         Library root directory
+            --cql                  <directory>         CQL root directory
+            [--fhir]               <file|directory>    Resource location, either file name or directory
+            [--cs]                 <file|directory>    C# output location, either file name or directory
+            [--d]                  <true|false>        Produce as a debug assembly
+            [--f]                  <true|false>        Force an overwrite even if the output file already exists
+            [--canonical-root-url] <url>               The root url used for the resource canonical.
+                                                       If omitted a '#' will be used.
+            [--fhir-override-date] <ISO8601-date-time> The UTC date to override in the generated FHIR resource libraries.
+                                                       (example: 2000-12-31T23:59:59.99Z)
+                                                       If omitted the current date time will be used.
         """;
 
 
