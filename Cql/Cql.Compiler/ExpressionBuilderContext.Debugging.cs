8686df38325628b71933d71a0decb6468c16210f
Paul den Boer <denboer.paul@gmail.com>
2024-03-07 08:19:28 +0100

    Split file

--- Cql/Cql.Compiler/ExpressionBuilderContext.Debugging.cs
+++ Cql/Cql.Compiler/ExpressionBuilderContext.Debugging.cs
@@ -1,8 +1,6 @@
-﻿using Hl7.Cql.Runtime;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Linq.Expressions;
 using System.Runtime.CompilerServices;
 using System.Text;
 using elm = Hl7.Cql.Elm;
@@ -133,47 +131,4 @@ partial class ExpressionBuilderContext
     public ExpressionBuildingException NewExpressionBuildingException(
         string? message = null, Exception? innerException = null) => 
         new(this, message, innerException);
-}
-
-
-internal class ExpressionBuilderContext<TElement> : ExpressionBuilderContext
-    where TElement : Elm.Element
-{
-    internal ExpressionBuilderContext(
-        ExpressionBuilder builder,
-        ParameterExpression contextParameter,
-        DefinitionDictionary<LambdaExpression> definitions,
-        IDictionary<string, string> localLibraryIdentifiers,
-        TElement element)
-        : base(builder, contextParameter, definitions, localLibraryIdentifiers)
-    {
-        Element = element ?? throw new ArgumentNullException(nameof(element));
-    }
-
-    protected ExpressionBuilderContext(
-        ExpressionBuilderContext source,
-        TElement element)
-        : base(source)
-    {
-        Element = element;
-    }
-
-    protected ExpressionBuilderContext(
-        ExpressionBuilderContext<TElement> source,
-        IDictionary<string, (Expression, elm.Element)>? overrideScopes = null)
-        : base(source, overrideScopes: overrideScopes)
-    {
-        Element = source.Element;
-    }
-
-    protected override ExpressionBuilderContext WithScopes(IDictionary<string, (Expression, elm.Element)> scopes) =>
-        new ExpressionBuilderContext<TElement>(this, scopes);
-
-    internal override ExpressionBuilderContext<TInnerElement> Deeper<TInnerElement>(TInnerElement element) =>
-        new(this, element);
-
-
-    public new TElement Element { get; }
-
-    protected override elm.Element GetElement() => Element;
 }
---
+++ Cql/Cql.Compiler/ExpressionBuilderContext.TElement.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using Hl7.Cql.Runtime;
+
+namespace Hl7.Cql.Compiler;
+
+internal class ExpressionBuilderContext<TElement> : ExpressionBuilderContext
+    where TElement : Elm.Element
+{
+    internal ExpressionBuilderContext(
+        ExpressionBuilder builder,
+        ParameterExpression contextParameter,
+        DefinitionDictionary<LambdaExpression> definitions,
+        IDictionary<string, string> localLibraryIdentifiers,
+        TElement element)
+        : base(builder, contextParameter, definitions, localLibraryIdentifiers)
+    {
+        Element = element ?? throw new ArgumentNullException(nameof(element));
+    }
+
+    protected ExpressionBuilderContext(
+        ExpressionBuilderContext source,
+        TElement element)
+        : base(source)
+    {
+        Element = element;
+    }
+
+    protected ExpressionBuilderContext(
+        ExpressionBuilderContext<TElement> source,
+        IDictionary<string, (Expression, Elm.Element)>? overrideScopes = null)
+        : base(source, overrideScopes: overrideScopes)
+    {
+        Element = source.Element;
+    }
+
+    protected override ExpressionBuilderContext WithScopes(IDictionary<string, (Expression, Elm.Element)> scopes) =>
+        new ExpressionBuilderContext<TElement>(this, scopes);
+
+    internal override ExpressionBuilderContext<TInnerElement> Deeper<TInnerElement>(TInnerElement element) =>
+        new(this, element);
+
+
+    public new TElement Element { get; }
+
+    protected override Elm.Element GetElement() => Element;
+}
