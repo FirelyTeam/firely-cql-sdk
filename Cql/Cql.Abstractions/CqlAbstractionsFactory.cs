8fd65c97674a7b4148c462a7d5fba4488d7beda3
Paul den Boer <denboer.paul@gmail.com>
2024-03-27 18:02:02 +0100

    Fix singleton creation in factory

--- Cql/Cql.Abstractions/CqlAbstractionsFactory.cs
+++ Cql/Cql.Abstractions/CqlAbstractionsFactory.cs
@@ -1,45 +1,11 @@
-﻿using System;
-using System.Threading;
+﻿using System.Threading;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 
 namespace Hl7.Cql.Abstractions;
 
-internal abstract class CqlFactoryBase
-{
-    protected CqlFactoryBase(ILoggerFactory loggerFactory) => LoggerFactory = loggerFactory;
-
-    protected ILoggerFactory LoggerFactory { get; }
-
-    protected virtual ILogger<T> Logger<T>() => Transient(LoggerFactory.CreateLogger<T>);
-
-    protected virtual IOptions<T> Options<T>(T options) where T : class => Singleton(() => Microsoft.Extensions.Options.Options.Create(options));
-
-    protected static T Transient<T>(Func<T> fn) => fn();
-
-    protected static T Singleton<T>(Func<T> fn)
-    {
-        var oldFn = Interlocked.CompareExchange(ref CachedSingleton<T>.CachedFunc, fn, null);
-
-        if (oldFn is null)
-            return CachedSingleton<T>.CachedValue = fn();
-
-        if (oldFn == fn)
-            return CachedSingleton<T>.CachedValue;
-
-        throw new InvalidOperationException("Not allowed to factory method for a given singleton type."); ;
-    }
-
-    private static class CachedSingleton<T>
-    {
-        public static Func<T>? CachedFunc;
-        public static T CachedValue = default!;
-    }
-}
-
 internal class CqlAbstractionsFactory : CqlFactoryBase
 {
-    public CqlAbstractionsFactory(ILoggerFactory loggerFactory) : base(loggerFactory)
+    protected CqlAbstractionsFactory(ILoggerFactory loggerFactory) : base(loggerFactory)
     {
     }
 }
---
+++ Cql/Cql.Abstractions/CqlFactoryBase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Concurrent;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace Hl7.Cql.Abstractions;
+
+internal abstract class CqlFactoryBase
+{
+    private readonly ConcurrentDictionary<Type, object> _singletons;
+
+    protected CqlFactoryBase(ILoggerFactory loggerFactory)
+    {
+        _singletons = new ConcurrentDictionary<Type, object>();
+        LoggerFactory = loggerFactory;
+    }
+
+    protected ILoggerFactory LoggerFactory { get; }
+
+    protected virtual ILogger<T> Logger<T>() => Transient(LoggerFactory.CreateLogger<T>);
+
+    protected virtual IOptions<T> Options<T>(T options) where T : class => Singleton(() => Microsoft.Extensions.Options.Options.Create(options));
+
+    protected virtual T Transient<T>(Func<T> fn) => fn();
+
+    protected virtual T Singleton<T>(Func<T> fn) => (T)_singletons.GetOrAdd(typeof(T), _ => fn()!);
+}
