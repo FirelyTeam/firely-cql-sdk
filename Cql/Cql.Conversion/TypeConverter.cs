770e5f4020079609ef99b22db4b6b6efd4a0710b
Paul den Boer <denboer.paul@gmail.com>
2024-05-06 15:47:31 +0200

    Update TypeConverter

--- Cql/Cql.Conversion/TypeConverter.cs
+++ Cql/Cql.Conversion/TypeConverter.cs
@@ -95,7 +95,8 @@ namespace Hl7.Cql.Conversion
         {
             if (from is null) return null;
             var fromType = from.GetType();
-            if (fromType == to) return from;
+            if (fromType.IsAssignableTo(to))
+                return from;
             
             if(_customConverters.SingleOrDefault(converter => converter.Handles(fromType, to)) is {} subConverter)
                 return subConverter.Convert(from, to);
