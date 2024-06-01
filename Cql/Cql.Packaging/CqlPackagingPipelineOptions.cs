d82efa96d863d8d00f013202eac1123542e99e1d
Paul den Boer <denboer.paul@gmail.com>
2024-06-01 14:46:43 +0200

    Flush and close logging on exit. Add flag for debug logging. Remove old debug and force flags (not used, must be implemented later)

--- Cql/Cql.Packaging/CqlPackagingPipelineOptions.cs
+++ Cql/Cql.Packaging/CqlPackagingPipelineOptions.cs
@@ -9,23 +9,20 @@ namespace Hl7.Cql.Packaging;
 
 internal partial class CqlToResourcePackagingOptions
 {
-    public const string ConfigSection = "Packager";
+	public const string ConfigSection = "Packager";
 
-    public const string ArgNameElmDirectory = "--elm";
-    public DirectoryInfo ElmDirectory { get; set; } = null!;
+	public const string ArgNameElmDirectory = "--elm";
+	public DirectoryInfo ElmDirectory { get; set; } = null!;
 
-    public const string ArgNameCqlDirectory = "--cql";
-    public DirectoryInfo CqlDirectory { get; set; } = null!;
+	public const string ArgNameCqlDirectory = "--cql";
+	public DirectoryInfo CqlDirectory { get; set; } = null!;
 
-    public const string ArgNameDebug = "--d";
-    public bool Debug { get; set; }
+	public const string ArgNameLogDebugEnabled = "--log-debug";
+	public bool LogDebugEnabled { get; set; }
 
-    public const string ArgNameForce = "--f";
-    public bool Force { get; set; } // TODO: There are no usages for this, Remove or implement!
+	public const string ArgNameLogDontClear = "--log-dont-clear";
+	internal bool DontLogClear { get; set; }  // Internal use only!
 
-    public const string ArgNameDontClearLog = "--log-dont-clear";
-    public bool DontClearLog { get; set; }  // Internal use only!
-
-    public const string ArgNameCanonicalRootUrl = "--canonical-root-url";
-    public Uri? CanonicalRootUrl { get; set; }
+	public const string ArgNameCanonicalRootUrl = "--canonical-root-url";
+	public Uri? CanonicalRootUrl { get; set; }
 }
