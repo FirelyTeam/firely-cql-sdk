86dbda57ad49e1a8e71404f9879c4128dae05d63
Paul den Boer <denboer.paul@gmail.com>
2024-08-20 09:14:39 +0200

    Regenerate demo code so the compilergeneratedattribute version number is correct from a previous PR

--- Demo/Measures.CMS/CSharp/ProstateCaAvoidanceBoneScanOveruseFHIR-0.2.000.g.cs
+++ Demo/Measures.CMS/CSharp/ProstateCaAvoidanceBoneScanOveruseFHIR-0.2.000.g.cs
@@ -10,7 +10,7 @@ using System.Reflection;
 using Hl7.Fhir.Model;
 using Range = Hl7.Fhir.Model.Range;
 using Task = Hl7.Fhir.Model.Task;
-[System.CodeDom.Compiler.GeneratedCode(".NET Code Generation", "2.0.0.0")]
+[System.CodeDom.Compiler.GeneratedCode(".NET Code Generation", "2.0.1.0")]
 [CqlLibrary("ProstateCaAvoidanceBoneScanOveruseFHIR", "0.2.000")]
 public class ProstateCaAvoidanceBoneScanOveruseFHIR_0_2_000
 {
