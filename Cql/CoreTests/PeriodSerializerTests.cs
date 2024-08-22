13a49f6a65ee32365ee99ddab2bb3a7ca1547f59
Paul den Boer <denboer.paul@gmail.com>
2024-08-22 13:02:05 +0200

    Investigate issue with a unit test, but not understanding the context yet

---
+++ Cql/CoreTests/PeriodSerializerTests.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Text.Json;
+using Hl7.Cql.Abstractions.Infrastructure;
+using Hl7.Fhir.Model;
+using Hl7.Fhir.Serialization;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CoreTests;
+
+[TestClass]
+public class PeriodSerializerTests
+{
+    [TestMethod]
+    public void Period_SerializeDefaultPeriodToJson_MustHaveValidFormat()
+    {
+        var fhirLibrary = new Library();
+        fhirLibrary.Parameter = new List<ParameterDefinition>
+        {
+            new()
+            {
+                Type = FHIRAllTypes.Period,
+            }
+        };
+
+        var jsonSerializerOptions = new JsonSerializerOptions().ForFhir(ModelInfo.ModelInspector);
+        var json = JsonSerializer.Serialize(fhirLibrary, jsonSerializerOptions);
+    }
+}
