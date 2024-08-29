6f40e295beffbcfd4cbc1f5eb22761b4df755d16
Paul den Boer <denboer.paul@gmail.com>
2024-08-29 16:08:18 +0200

    Fix safe casting from base to derived

--- Cql/Cql.Compiler/ExpressionBuilderContext.cs
+++ Cql/Cql.Compiler/ExpressionBuilderContext.cs
@@ -2172,24 +2172,28 @@ partial class ExpressionBuilderContext
                 {
                     var type = TypeFor(@as.asTypeSpecifier!);
                     var operand = TranslateArg(@as.operand!);
-                    var operandType = operand.Type;
-                    var typeConversion = TypeConversion.NoMatch;
-                    var converted = ChangeType(operand, type, out typeConversion);
-                    if (typeConversion == TypeConversion.NoMatch)
+                    var converted = ChangeType(operand, type, out var typeConversion, safeUpcastAllowed:true);
+                    switch (typeConversion)
                     {
-                        // log an unsafe cast
-                        _logger.LogWarning(FormatMessage(
-                            $"{operand.Type.ToCSharpString(Defaults.TypeCSharpFormat)} as {type.ToCSharpString(Defaults.TypeCSharpFormat)} will always result in null.",
-                            @as.operand));
-                        return Expression.Default(type);
-                    }
-                    else if (typeConversion == TypeConversion.OperatorConvert) // use the custom conversion function
-                    {
-                        return converted;
-                    }
-                    else
-                    {
-                        return new ElmAsExpression(operand, type, @as.strict);
+                        case TypeConversion.NoMatch:
+                            // log an unsafe cast
+                            _logger.LogWarning(
+                                FormatMessage(
+                                    $"{operand.Type.ToCSharpString(Defaults.TypeCSharpFormat)} as {type.ToCSharpString(Defaults.TypeCSharpFormat)} will always result in null.",
+                                    @as.operand));
+
+                            return Expression.Default(type);
+
+                        // use the custom conversion function
+                        case TypeConversion.OperatorConvert:
+                            return converted;
+
+                        case TypeConversion.ExpressionTypeAs:
+                            return converted;
+                            //return new ElmAsExpression(operand, type, @as.strict);
+
+                        default:
+                            return new ElmAsExpression(operand, type, @as.strict);
                     }
                 }
             }
@@ -2297,9 +2301,10 @@ partial class ExpressionBuilderContext
     private Expression ChangeType(
         Expression input,
         Type outputType,
-        out TypeConversion typeConversion) // @TODO: Cast - ChangeType
+        out TypeConversion typeConversion,
+        bool safeUpcastAllowed = false) // @TODO: Cast - ChangeType
     {
-        var (expression, tc) = input.TryNewAssignToTypeExpression(outputType, false);
+        var (expression, tc) = input.TryNewAssignToTypeExpression(outputType, false, safeUpcastAllowed);
         if (tc != TypeConversion.NoMatch)
         {
             typeConversion = tc;
--- Cql/Cql.Compiler/TypeConversion.cs
+++ Cql/Cql.Compiler/TypeConversion.cs
@@ -1,24 +1,27 @@
-﻿// /*
-//  * Copyright (c) 2024, NCQA and contributors
-//  * See the file CONTRIBUTORS for details.
-//  *
-//  * This file is licensed under the BSD 3-Clause license
-//  * available at https://raw.githubusercontent.com/FirelyTeam/firely-cql-sdk/main/LICENSE
-//  */
+﻿/*
+ * Copyright (c) 2024, NCQA and contributors
+ * See the file CONTRIBUTORS for details.
+ *
+ * This file is licensed under the BSD 3-Clause license
+ * available at https://raw.githubusercontent.com/FirelyTeam/firely-cql-sdk/main/LICENSE
+ */
 
 namespace Hl7.Cql.Compiler;
 
 internal enum TypeConversion
 {
+    // Type 'Source' is not convertible to type 'Target'
     NoMatch = 0,
+
+    // Type 'Source' is exactly type 'Target'
     ExactType = 1,
 
-    /// <summary>
-    /// e.g. String is assignable to Object, and  'from' is a subtype of 'to' e.g. string -> object
-    /// </summary>
+    // Can cast 'Source' to 'Target' because 'Source' derives from 'Target'. e.g. (Target)source; // Casting down
     ExpressionCast = 2,
 
+    // Can safely cast 'Source' as 'Target' because 'Source' derives from 'Target' e.g. (source as Target) // Casting up
     ExpressionTypeAs = 3,
 
+    // No direct cast from 'Source' to 'Target', a cql operator is needed
     OperatorConvert = 4,
 }
