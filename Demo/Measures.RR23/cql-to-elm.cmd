cee426610a8805acb193599f1f22ae69f9235e5d
Paul den Boer <denboer.paul@gmail.com>
2024-09-24 12:51:24 +0200

    Rename folder

---
+++ Demo/Measures.RR23/cql-to-elm.cmd
@@ -0,0 +1,7 @@
+"..\..\Cql\PackagerCLI\bin\Debug\net8.0\Hl7.Cql.Packager.exe" \
+  --cql "./input/cql"
+  --elm "./Elm"
+  --cq "./CSharp"
+  --fhir "./Resources"
+  --override-utc-date-time "1970-01-01T00:00:00.000Z" \
+  --canonical-root-url "https://fire.ly/fhir/"
