a707205b3a5c044b72cfc1bd625d9ce4e7992627
Andreas Hussong <andreas.hussong@philips.com>
2024-10-08 20:40:39 +0200

    Add test for nested tuple

--- Cql/CoreTests/CodeGenerationNet/CqlTupleTest.cs
+++ Cql/CoreTests/CodeGenerationNet/CqlTupleTest.cs
@@ -36,4 +36,30 @@ public class CqlTupleTest
         var str = JsonSerializer.Serialize(obj);
         Assert.AreEqual("{\"status\":\"success\",\"result\":\"some result\"}", str);
     }
+
+    [TestMethod]
+    public void Result_ShouldBeDynamicWithNestedNamedItems()
+    {
+        var file = @"Assemblies/CqlNestedTupleTest-1.0.0.dll";
+        var filePath = Path.GetFullPath(file);
+
+        var asm = new AssemblyLoadContext("CqlNestedTupleTest");
+        asm.LoadFromAssemblyPath(filePath);
+
+        var ctx = FhirCqlContext.ForBundle();
+        var result = asm.Run("CqlNestedTupleTest", "1.0.0", ctx);
+        Assert.IsNotNull(result);
+
+        result.TryGetValue("Result", out dynamic obj);
+        Assert.IsNotNull(obj);
+        Assert.IsInstanceOfType(obj, typeof(ExpandoObject));
+
+        Assert.AreEqual("success", obj.status);
+        Assert.AreEqual("some first result", obj.result.result1);
+        Assert.AreEqual("some second result", obj.result.result2);
+
+        // serialization works out of the box
+        var str = JsonSerializer.Serialize(obj);
+        Assert.AreEqual("{\"status\":\"success\",\"result\":\"some result\"}", str);
+    }
 }
