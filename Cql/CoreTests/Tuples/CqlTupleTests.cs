08ba46713034bacd90a2d8dfc4bcc51ed049afe3
Paul den Boer <denboer.paul@gmail.com>
2024-10-15 15:03:31 +0200

    Fix

--- Cql/CoreTests/Tuples/CqlTupleTests.cs
+++ Cql/CoreTests/Tuples/CqlTupleTests.cs
@@ -1,64 +1,36 @@
 ﻿#nullable enable
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text.Json;
-using System.Text.Json.Serialization;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-
+using System.Text.Json;
+using Hl7.Cql.Runtime;
+using Hl7.Cql.Runtime.Serialization;
 
 namespace CoreTests.Tuples;
-
-#region Tests
 
 [TestClass]
 public class CqlTupleTests
 {
     // Generated C# as part of Library
-    public static readonly string[] TupleOnLibraryAProperties = ["Name", "DOB"];
-    public static readonly string[] TupleOnLibraryBProperties = ["Name", "DOB"];
-
-    [TestMethod]
-    public void TestEquality()
-    {
-        // Generated C# as part of Library
-        CqlTuple<(string? Name, DateTime? DOB)> tupleA1 = new(TupleOnLibraryAProperties, ("Paul", new DateTime(2000, 12, 31)));
-        CqlTuple<(string? Name, DateTime? DOB)> tupleA2 = new(TupleOnLibraryBProperties, ("Paul", new DateTime(2000, 12, 31)));
-
-
-        Assert.AreEqual(tupleA1, tupleA2);
-    }
-
-    [TestMethod]
-    public void TestJsonSerialization()
-    {
-        // Generated C# as part of Library
-        CqlTuple<(string? Name, DateTime? DOB)> tupleA1 = new(TupleOnLibraryAProperties, ("Paul", new DateTime(2000, 12, 31)));
-
-
-        var serializedJson = JsonSerializer.Serialize(tupleA1);
-        Assert.AreEqual("""{"Name":"Paul","DOB":"2000-12-31T00:00:00"}""", serializedJson);
-    }
-
-    public static readonly string[] PersonProperties = ["Name", "ID", "Addresses"];
-    public static readonly string[] AddressProperties = ["AddressType", "Street", "City", "Country"];
+    public static readonly CqlTupleMetadata PersonProperties = new([typeof(string), typeof(int?), typeof((CqlTupleMetadata, string?, string?, string?)[])], ["Name", "ID", "Addresses"]);
+    public static readonly CqlTupleMetadata AddressProperties = new ([typeof(string), typeof(string), typeof(string)], ["AddressType", "Street", "City", "Country"]);
 
     [TestMethod]
     public void TestJsonSerializationNested()
     {
         // Generated C# as part of Library
-        CqlTuple<(string? AddressType, string? Street, string? City, string? Country)> homeAddr =
-            new(AddressProperties, ( "Home", "Joe Street", "Springfield", "USA"));
+        (CqlTupleMetadata, string? AddressType, string? Street, string? City, string? Country) homeAddr =
+            (AddressProperties, "Home", "Joe Street", "Springfield", "USA");
 
-        CqlTuple<(string? AddressType, string? Street, string? City, string? Country)> workAddr =
-            new(AddressProperties, ("Work", "Sue Street", "Jumpville", "Canada"));
+        (CqlTupleMetadata, string? AddressType, string? Street, string? City, string? Country) workAddr =
+            (AddressProperties, "Work", "Sue Street", "Jumpville", "Canada");
 
-        CqlTuple<(string? Name, int? ID, CqlTuple<(string? AddressType, string? Street, string? City, string? Country)>[]? Addressses)> person =
-            new(PersonProperties, ("John", 10, [homeAddr, workAddr]));
+        (CqlTupleMetadata, string? Name, int? ID, (CqlTupleMetadata, string? AddressType, string? Street, string? City, string? Country)[]? addressses) person =
+            (PersonProperties, "John", 10, [homeAddr, workAddr]);
+
+        var serializedJson = JsonSerializer.Serialize(person, new JsonSerializerOptions {
+            Converters = { new CqlValueTupleJsonConverterFactory() },
+            WriteIndented = true });
 
 
-        var serializedJson = JsonSerializer.Serialize(person, new JsonSerializerOptions { WriteIndented = true});
         Assert.AreEqual(
             """
             {
@@ -82,82 +54,3 @@ public class CqlTupleTests
             """, serializedJson);
     }
 }
-
-#endregion
-
-#region Serializers (Runtime)
-
-public class CqlTupleJsonConverterFactory : JsonConverterFactory
-{
-    public override bool CanConvert(Type typeToConvert)
-    {
-        return typeToConvert.IsGenericType
-               && typeToConvert.GetGenericTypeDefinition() == typeof(CqlTuple<>);
-    }
-
-    public override JsonConverter? CreateConverter(Type typeToConvert, JsonSerializerOptions options)
-    {
-        var tupleType = typeToConvert.GetGenericArguments()[0] ??
-                        throw new InvalidOperationException("This converter only handles array types.");
-
-        var type = typeof(CqlTupleJsonConverter<>).MakeGenericType(tupleType);
-        var instance = (JsonConverter)Activator.CreateInstance(type)!;
-        return instance;
-    }
-}
-
-public class CqlTupleJsonConverter<TTuple> : JsonConverter<CqlTuple<TTuple>> where TTuple : struct, ITuple
-{
-    public override CqlTuple<TTuple> Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
-    {
-        throw new NotImplementedException();
-    }
-
-    public override void Write(Utf8JsonWriter writer, CqlTuple<TTuple> value, JsonSerializerOptions options)
-    {
-        writer.WriteStartObject();
-
-        INamedPropertiesTuple namedPropertiesTuple = value;
-        for (int i = 0; i < namedPropertiesTuple.Properties.Length; i++)
-        {
-            writer.WritePropertyName(namedPropertiesTuple.Properties[i]);
-            JsonSerializer.Serialize(writer, namedPropertiesTuple[i], options);
-        }
-
-        writer.WriteEndObject();
-    }
-}
-
-#endregion
-
-#region Runtime SDK
-
-public interface INamedPropertiesTuple : ITuple
-{
-    string[] Properties { get; }
-}
-
-[JsonConverter(typeof(CqlTupleJsonConverterFactory))]
-public readonly struct CqlTuple<TTuple>(
-    string[] properties,
-    TTuple value) : IEquatable<CqlTuple<TTuple>>, INamedPropertiesTuple
-    where TTuple : struct, ITuple
-{
-    private string[] Properties { get; } = properties;
-
-    public TTuple Value { get; } = value;
-
-    public override bool Equals(object? obj) => obj is CqlTuple<TTuple> tuple && Equals(tuple);
-
-    public bool Equals(CqlTuple<TTuple> other) => Properties.SequenceEqual(other.Properties) && EqualityComparer<TTuple>.Default.Equals(Value, other.Value);
-
-    public override int GetHashCode() => HashCode.Combine(Properties, Value);
-
-    object? ITuple.this[int index] => Value[index];
-
-    int ITuple.Length => Value.Length;
-
-    string[] INamedPropertiesTuple.Properties => Properties;
-}
-
-#endregion
