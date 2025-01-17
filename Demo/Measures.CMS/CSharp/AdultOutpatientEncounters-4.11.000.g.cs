188e1351cab67bbf66e57c9b5220945d4b55b6d0
Paul den Boer <denboer.paul@gmail.com>
2025-01-17 10:21:50 +0100

    .

--- Demo/Measures.CMS/CSharp/AdultOutpatientEncounters-4.11.000.g.cs
+++ Demo/Measures.CMS/CSharp/AdultOutpatientEncounters-4.11.000.g.cs
@@ -12,7 +12,7 @@ using Hl7.Fhir.Model;
 using Range = Hl7.Fhir.Model.Range;
 using Task = Hl7.Fhir.Model.Task;
 
-[System.CodeDom.Compiler.GeneratedCode(".NET Code Generation", "2.0.10.0")]
+[System.CodeDom.Compiler.GeneratedCode(".NET Code Generation", "2.0.11.0")]
 [CqlLibrary("AdultOutpatientEncounters", "4.11.000")]
 public partial class AdultOutpatientEncounters_4_11_000 : ILibrary, ISingleton<AdultOutpatientEncounters_4_11_000>
 {
--- Demo/Measures.CMS/CSharp/IntravesicalBacillusCalmetteGuerinForBladderCancerFHIR-1.4.000.g.cs
+++ Demo/Measures.CMS/CSharp/IntravesicalBacillusCalmetteGuerinForBladderCancerFHIR-1.4.000.g.cs
@@ -12,7 +12,7 @@ using Hl7.Fhir.Model;
 using Range = Hl7.Fhir.Model.Range;
 using Task = Hl7.Fhir.Model.Task;
 
-[System.CodeDom.Compiler.GeneratedCode(".NET Code Generation", "2.0.10.0")]
+[System.CodeDom.Compiler.GeneratedCode(".NET Code Generation", "2.0.11.0")]
 [CqlLibrary("IntravesicalBacillusCalmetteGuerinForBladderCancerFHIR", "1.4.000")]
 public partial class IntravesicalBacillusCalmetteGuerinForBladderCancerFHIR_1_4_000 : ILibrary, ISingleton<IntravesicalBacillusCalmetteGuerinForBladderCancerFHIR_1_4_000>
 {
