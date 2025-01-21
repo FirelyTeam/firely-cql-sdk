9e21b36fde9d569335e66407db347556a22ecd64
Paul den Boer <denboer.paul@gmail.com>
2025-01-21 14:44:18 +0100

    Fix assertion

---
+++ Cql/CoreTests/AssertExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CoreTests;
+
+internal static class AssertExtensions
+{
+    public static void DoesNotThrow(
+        this Assert assert,
+        Action action)
+    {
+        try
+        {
+            action();
+        }
+        catch (Exception e)
+        {
+            throw new AssertFailedException("Assertion failed, an exception was not supposed to be thrown. See InnerException for details. ", e);
+        }
+    }
+
+}
