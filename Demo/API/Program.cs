5fb611b346901d23dfac9c3c14140596dec09fb2
Paul den Boer <denboer.paul@gmail.com>
2025-01-21 22:11:10 +0100

    Update example

--- Demo/API/Program.cs
+++ Demo/API/Program.cs
@@ -1,7 +1,7 @@
 ﻿using CqlSdkPrototype.Cql;
 using CqlSdkPrototype.Cql.Extensions;
 using CqlSdkPrototype.Elm.Extensions;
-using CqlSdkPrototype.Runtime.Extensions;
+using CqlSdkPrototype.Invocation.Extensions;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
@@ -13,26 +13,32 @@ internal static class Program
     {
         //Setup DI
         var services = new ServiceCollection();
-
         services.AddLogging(builder => builder.AddConsole());
+        services.AddSingleton<CqlApiOptions>(new CqlApiOptions(Models: [CqlModel.ElmR1, CqlModel.Fhir401]));
+        services.AddSingleton<CqlApi>();
+        services.AddSingleton<Example1>();
 
-        var cqlApiOptions = new CqlApiOptions(Models: [CqlModel.ElmR1, CqlModel.Fhir401]);
-        services.AddSingleton<CqlApi>(provider => ActivatorUtilities.CreateInstance<CqlApi>(provider, cqlApiOptions));
-
-        // Get CqlApi from DI
         IServiceProvider serviceProvider = services.BuildServiceProvider();
-        var cqlApi = serviceProvider.GetRequiredService<CqlApi>();
-
-        // Setup and use CqlApi
-        cqlApi.AddCqlLibrariesFromDirectory(new DirectoryInfo("input/cql/"));
-        cqlApi.Translate();
-        cqlApi.SaveElmFileToDirectory(new DirectoryInfo("output/elm/"));
-
-        // Setup and use ElmApi
-        var elmApi = cqlApi.CreateElmApi();
-        elmApi.Compile();
-        elmApi.SaveCSharpFilesToDirectory(new DirectoryInfo("output/csharp/"));
-        elmApi.SaveAssemblyBinariesToDirectory(new DirectoryInfo("output/assemblies/"));
+        var example1 = serviceProvider.GetRequiredService<Example1>();
+        example1.Run();
     }
 }
 
+internal class Example1(CqlApi cqlApi)
+{
+    public void Run()
+    {
+        // Setup and use CqlApi
+        cqlApi
+            .AddCqlLibrariesFromDirectory(new DirectoryInfo("input/cql/"))
+            .Translate()
+            .SaveElmFileToDirectory(new DirectoryInfo("output/elm/"));
+
+        // Setup and use ElmApi
+        var elmApi = cqlApi.CreateElmApi();
+        elmApi
+            .Compile()
+            .SaveCSharpFilesToDirectory(new DirectoryInfo("output/csharp/"))
+            .SaveAssemblyBinariesToDirectory(new DirectoryInfo("output/assemblies/"));
+    }
+}
