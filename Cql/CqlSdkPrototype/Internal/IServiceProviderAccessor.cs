dfd7f0a930f9763ab2bacea079cf9e5c9dd0df5f
Paul den Boer <denboer.paul@gmail.com>
2025-02-04 11:06:40 +0100

    Remove empty files

--- Cql/CqlSdkPrototype/Internal/IServiceProviderAccessor.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace CqlSdkPrototype.Internal;
-
-internal interface IServiceProviderAccessor<TApi>
-{
-    ServiceProvider ServiceProvider { get; }
-}
--- Cql/CqlSdkPrototype/Invocation/LibrarySetInvokerBuilderConfig.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace CqlSdkPrototype.Invocation;
-
-/*public sealed record LibrarySetInvokerBuilderConfig()
-{
-    public static LibrarySetInvokerBuilderConfig Default { get; } = new();
-}*/
