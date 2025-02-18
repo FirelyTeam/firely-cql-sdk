526ff2e7e1b6ac67e7d2ffea1b36eb34c1bdbb2e
Paul den Boer <denboer.paul@gmail.com>
2025-02-18 16:25:01 +0100

    .

--- Cql/Elm/Elm.cs
+++ Cql/Elm/Elm.cs
@@ -33,6 +33,17 @@ internal static class IGetVersionedIdentifierExtensions
             (_, { } error) when throwError => throw error,
             _                              => null
         };
+
+    public static CqlVersionedLibraryIdentifier ToCqlVersionedLibraryIdentifier(this VersionedIdentifier identifier)
+    {
+        // We have to check for nulls because the generated ELM code does not emit nullability annotations.
+        ArgumentNullException.ThrowIfNull(identifier);
+        ArgumentNullException.ThrowIfNull(identifier.id);
+
+        var cqlLibraryIdentifier = CqlLibraryIdentifier.Parse(identifier.id);
+        CqlLibraryVersion? cqlLibraryVersion = identifier.version is null ? null : CqlLibraryVersion.Parse(identifier.version);
+        return CqlVersionedLibraryIdentifier.FromNameAndVersion(cqlLibraryIdentifier, cqlLibraryVersion);
+    }
 }
 
 /// <summary>
