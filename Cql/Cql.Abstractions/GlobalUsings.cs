3386286b016f85bb563c086e35ce6579327b6003
Paul den Boer <denboer.paul@gmail.com>
2025-02-20 11:34:31 +0100

    Move Primitives into folder

--- Cql/Cql.Abstractions/GlobalUsings.cs
+++ Cql/Cql.Abstractions/GlobalUsings.cs
@@ -6,6 +6,7 @@ global using System.Collections.Generic;
 global using System.Collections.Immutable;
 global using System.Diagnostics;
 global using System.Diagnostics.CodeAnalysis;
+global using System.Globalization;
 global using System.IO;
 global using System.Linq;
 global using System.Linq.Expressions;
