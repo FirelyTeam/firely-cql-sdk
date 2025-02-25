29deac5257b0c057af89814c8ef11eedde8a8882
richfirely <rich@fire.ly>
2025-02-25 17:52:12 -0500

    resource extensions adjustment

--- Cql/Cql.Packaging/ResourceExtensions.cs
+++ Cql/Cql.Packaging/ResourceExtensions.cs
@@ -15,7 +15,6 @@ internal static class ResourceExtensions
     {
         if (string.IsNullOrWhiteSpace(resource.Id))
             throw new ArgumentException("Resource must have an id", nameof(resource));
-
         string root = resourceCanonicalRootUrl ?? "#";
         var path = $"{root}{(root.EndsWith('/') ? "" : "/")}{resource.TypeName}/{resource.Id}";
         return path;
@@ -25,9 +24,9 @@ internal static class ResourceExtensions
     {
         ResourceFileName resourceFileName = resource switch
         {
-            Library l => ResourceFileName.Create(nameof(Library), l.Id, l.Version),
-            Measure m => ResourceFileName.Create(nameof(Measure), m.Id, m.Version),
-            _         => throw new UnreachableException("Only expecting Library or Measure.")
+            Library l => ResourceFileName.Create(nameof(Library), l.Name, l.Version),
+            Measure m => ResourceFileName.Create(nameof(Measure), m.Name, m.Version),
+            _ => throw new UnreachableException("Only expecting Library or Measure.")
         };
         return resourceFileName;
     }
