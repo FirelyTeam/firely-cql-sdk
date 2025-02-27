0dde3276f1cb2db575c65327b1ea3260800aac8b
richfirely <rich@fire.ly>
2025-02-27 10:08:40 -0500

    cleanup extensions and tests.

--- Cql/Cql.Packaging/ResourceExtensions.cs
+++ Cql/Cql.Packaging/ResourceExtensions.cs
@@ -11,15 +11,6 @@ namespace Hl7.Cql.Packaging;
 
 internal static class ResourceExtensions
 {
-    public static string CanonicalUri(this Resource resource, string? resourceCanonicalRootUrl)
-    {
-        if (string.IsNullOrWhiteSpace(resource.Id))
-            throw new ArgumentException("Resource must have an id", nameof(resource));
-        string root = resourceCanonicalRootUrl ?? "#";
-        var path = $"{root}{(root.EndsWith('/') ? "" : "/")}{resource.TypeName}/{resource.Id}";
-        return path;
-    }
-
     public static ResourceFileName GetResourceFileName(this Resource resource)
     {
         ResourceFileName resourceFileName = resource switch
