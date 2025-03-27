5c17f642692e4a3a36fc3cd20f1aefe6c6abfa09
Paul den Boer <denboer.paul@gmail.com>
2025-03-27 23:49:58 +0100

    .

--- Cql/Cql.Abstractions/Abstractions/ICqlComparer.cs
+++ Cql/Cql.Abstractions/Abstractions/ICqlComparer.cs
@@ -1,12 +1,11 @@
 ﻿/*
- * Copyright (c) 2023, NCQA and contributors
+ * Copyright (c) 2023, Firely, NCQA and contributors
  * See the file CONTRIBUTORS for details.
  *
  * This file is licensed under the BSD 3-Clause license
  * available at https://raw.githubusercontent.com/FirelyTeam/firely-cql-sdk/main/LICENSE
  */
 
-#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
 namespace Hl7.Cql.Abstractions
 {
     /// <summary>
