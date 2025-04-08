cdc74eeca0a2856d7e777e17b80ddfd710abc9ab
Paul den Boer <denboer.paul@gmail.com>
2025-04-08 13:16:30 +0200

    Remove dead code and related options

--- Cql/Cql.CqlToElm/CqlToElmOptions.cs
+++ Cql/Cql.CqlToElm/CqlToElmOptions.cs
@@ -20,29 +20,6 @@ namespace Hl7.Cql.CqlToElm
 
         /// <summary>
         /// <para>
-        /// The root path where all input libraries will be located.
-        /// </para>
-        /// <para>
-        /// This path may contain CQL files, JSON ELM libraries, and XML ELM libraries.
-        /// </para>
-        /// </summary>
-        public string? Input { get; set; }
-
-        /// <summary>
-        /// <para>
-        /// When <see langword="true"/>, files in the <see cref="Input"/> path with a .cql extension will be treated as CQL source files
-        /// and files with a .json or .xml extension will be treated as ELM libraries.  All other files will be ignored.
-        /// When <see langword="false"/>, all files in <see cref="Input"/> will be analyzed to determine which type of artifact it is.
-        /// The default value is <see langword="true"/>.
-        /// </para>
-        /// <para>
-        /// Setting this value to <see langword="false"/> can have a significant adverse performance impact for paths with many files.
-        /// </para>
-        /// </summary>
-        public bool? AssumeInputExtensions { get; set; } = true;
-
-        /// <summary>
-        /// <para>
         /// Sets the default URI of the ELM model that will be automatically included
         /// as a <code>using</code> directive, using the local identifer <code>System</code>.
         /// </para>
@@ -149,20 +126,6 @@ namespace Hl7.Cql.CqlToElm
         /// </remarks>
         /// <seealso href="https://build.fhir.org/ig/HL7/cql/06-translationsemantics.html#disable-list-promotion-and-demotion" />
         public bool? EnableListDemotion { get; set; } = false;
-
-        /// <summary>
-        /// <para>
-        /// When <see langword="true"/>, redefining a previously defined identifier will be allowed, for example:
-        /// <code>
-        /// from [Condition] c let x: c.onset, x: c.abatement return x
-        /// </code>
-        /// </para>
-        /// <para>
-        /// When <see langword="false"/>, an error will occur.
-        /// The default value is <see langword="false"/>.
-        /// </para>
-        /// </summary>
-        public bool? AllowScopeRedefinition { get; set; } = false;
 
         /// <summary>
         /// <para>
