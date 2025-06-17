d6d3b5965b5c408cb372982d5d329177d2543102
Paul den Boer <denboer.paul@gmail.com>
2025-06-17 15:30:36 +0200

    .

--- Cql/Cql.Compiler/ExpressionBuilderContext.cs
+++ Cql/Cql.Compiler/ExpressionBuilderContext.cs
@@ -551,6 +551,7 @@ partial class ExpressionBuilderContext
 
         // Find a constructor that matches the provided parameters.
         const int NOT_MAPPED_HAS_DEFAULT_VALUE = int.MaxValue;
+        const byte B_FALSE = 0x0, B_TRUE = 0x1;
         var (ctor, ctorParameters, ctorPositionToParameterPositionMap) =
             // Prefer constructors with more parameters, and
             instanceType.GetConstructors()
@@ -567,7 +568,7 @@ partial class ExpressionBuilderContext
                             // is not assignable from the provided value by name or type
                             // when the parameter has no default value.
                             int[] ctorPositionToParameterPositionMap = new int[ctorParameters.Length];
-                            Span<bool> isParameterNameValuePairMapped = stackalloc bool[parameterNameValuePairs.Length];
+                            Span<byte> isParameterNameValuePairMapped = stackalloc byte[parameterNameValuePairs.Length];
                             for (var i = 0; i < ctorParameters.Length; i++)
                             {
                                 var ctorParameter = ctorParameters[i];
@@ -585,13 +586,13 @@ partial class ExpressionBuilderContext
                                         return default; // Exit immediately if we cannot map this parameter
                                     case { } p:
                                         ctorPositionToParameterPositionMap[i] = p;
-                                        isParameterNameValuePairMapped[parameterPosition] = true;
+                                        isParameterNameValuePairMapped[parameterPosition] = B_TRUE;
                                         break;
                                 }
                             }
 
                             // Make sure there are no provided values that are not mapped to a constructor parameter
-                            if (isParameterNameValuePairMapped.Contains(false))
+                            if (isParameterNameValuePairMapped.Contains(B_FALSE))
                                 return default;
 
                             return (true, (ctor, ctorParameters, ctorPositionToParameterPositionMap));
