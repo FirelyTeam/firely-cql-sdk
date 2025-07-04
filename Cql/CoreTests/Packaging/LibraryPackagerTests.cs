906fb565dca3021fbafb7b3bdd700522a4a294b9
Paul den Boer <denboer.paul@gmail.com>
2025-07-04 23:37:38 +0200

    Add unit tests for LibraryPackager

--- Cql/CoreTests/Packaging/LibraryPackagerTests.cs
+++ Cql/CoreTests/Packaging/LibraryPackagerTests.cs
@@ -1,8 +1,13 @@
 using Hl7.Cql.Compiler;
+using Hl7.Cql.Elm;
 using Hl7.Cql.Fhir;
+using Hl7.Cql.Iso8601;
 using Hl7.Cql.Packaging;
+using Hl7.Cql.Runtime;
 using Hl7.Fhir.Model;
 using Microsoft.Extensions.Logging.Abstractions;
+using System.Diagnostics;
+using DateTime = System.DateTime;
 using Library = Hl7.Cql.Elm.Library;
 
 namespace CoreTests.Packaging;
@@ -11,7 +16,89 @@ namespace CoreTests.Packaging;
 public class LibraryPackagerTests
 {
     private static readonly FhirTypeResolver TypeResolver = new (ModelInfo.ModelInspector);
-    private readonly CqlTypeToFhirTypeMapper _mapper = new (TypeResolver);
+    private readonly CqlTypeToFhirTypeMapper _mapper = new(TypeResolver);
+
+    private static readonly VersionedIdentifier VersionedIdentifier = new() { id = "test-lib", system = "name.space", version = "1.2.3" };
+    private static readonly byte[] ElmBytes = [0xff, 0x00, 0x00, 0x01];
+    private static readonly byte[] CqlBytes = [0xff, 0x00, 0x00, 0x10];
+    private static readonly byte[] AssemblyBytes = [0xff, 0x00, 0x01, 0x00];
+    private static readonly byte[] DebugSymbolBytes = [0xff, 0x00, 0x10, 0x00];
+    private static readonly DateTime Date = new(2001, 2, 3, 4, 5, 6, DateTimeKind.Local);
+    private static readonly string TestUrl = "test.firely";
+    private static readonly string CSharpString = "namespace Test {}";
+    private static readonly string CSharpFileName = "dummy.g.cs";
+
+    [TestMethod]
+    public void CreateLibraryResource_TestProperties()
+    {
+        // Arrange/Act
+        var library = CreateFhirLibrary();
+
+        // Assert
+        Assert.AreEqual($"7a03c0bb-{VersionedIdentifier.id}-{VersionedIdentifier.version}", library.Id);
+        Assert.AreEqual("Library", library.TypeName);
+        Assert.AreEqual(VersionedIdentifier.id, library.Name);
+        Assert.AreEqual(VersionedIdentifier.id, library.Title);
+        Assert.AreEqual(null, library.Description);
+        Assert.AreEqual(VersionedIdentifier.version, library.Version);
+        var expectedDate = new DateTimeIso8601(Date.ToLocalTime(), Hl7.Cql.Iso8601.DateTimePrecision.Millisecond).ToString();
+        Assert.AreEqual(expectedDate, library.Date);
+        Assert.AreEqual(expectedDate, library.Url);
+    }
+
+    [TestMethod]
+    public void CreateLibraryResource_TestAttachments()
+    {
+        // Arrange/Act
+        var library = CreateFhirLibrary();
+
+        // Assert
+        var attachmentsByElementId =
+            library.Content.ToDictionary(c => c.ElementId, c => (c.ContentType, c.Data));
+
+        string id = new CqlVersionedLibraryIdentifier(
+            CqlLibraryIdentifier.NewVerbatim(VersionedIdentifier.id),
+            CqlLibraryVersion.NewVerbatim(VersionedIdentifier.version))
+            .ToString();
+
+        Assert.AreEqual(("application/elm+json", ElmBytes), RemoveByElementId($"{id}+elm"));
+        Assert.AreEqual(("text/cql", CqlBytes), RemoveByElementId($"{id}+cql"));
+        Assert.AreEqual(("application/octet-stream", AssemblyBytes), RemoveByElementId($"{id}+dll"));
+        Assert.AreEqual(("application/octet-stream", DebugSymbolBytes), RemoveByElementId($"{id}+pdb"));
+        var cSharpAttachment = RemoveByElementId($"{CSharpFileName}+csharp");
+        Assert.AreEqual(("text/plain", CSharpString), (cSharpAttachment.contentType, Encoding.UTF8.GetString(cSharpAttachment.data)));
+        Assert.IsEmpty(attachmentsByElementId, "additional attachments that were not tested for, were found.");
+
+        (string contentType, byte[] data) RemoveByElementId(string elementId)
+        {
+            if (attachmentsByElementId.Remove(elementId, out var removed))
+                return removed;
+
+            Assert.Fail($"The elementId {elementId} was not found in the library attachments.");
+            throw new UnreachableException();
+        }
+    }
+
+    private Hl7.Fhir.Model.Library CreateFhirLibrary()
+    {
+        var versionedIdentifier = VersionedIdentifier;
+        var elmLibrary = new Library(versionedIdentifier);
+        // Act
+        var library = LibraryPackager.CreateLibraryResource(
+            NullLogger.Instance,
+            typeCrosswalk: _mapper,
+            elmLibrary: elmLibrary,
+            elmBytes: ElmBytes,
+            cqlBytes: CqlBytes,
+            assemblyBytes: AssemblyBytes,
+            debugSymbols: DebugSymbolBytes,
+            cSharpSourceCodeById: [new (CSharpFileName, CSharpString)],
+            elmLibrarySet: new LibrarySet("", [elmLibrary]),
+            resourceCanonicalBuilder: (_, _, _) => TestUrl,
+            elmFileLastWriteTimeUtc: Date);
+        Assert.IsNotNull(library);
+        return library;
+    }
 
     [DataTestMethod]
     [DataRow("Input/ELM/Test/ParameterTest_ResultTypeSpecifier.json", FHIRAllTypes.Integer)]
@@ -24,7 +111,7 @@ public class LibraryPackagerTests
         var elmLibrary = Library.LoadFromJson(new FileInfo(filename));
         // Act
         var library = LibraryPackager.CreateLibraryResource(
-            NullLoggerFactory.Instance.CreateLogger("Dummy"),
+            NullLogger.Instance,
             typeCrosswalk: _mapper,
             elmLibrary: elmLibrary,
             elmBytes: File.ReadAllBytes(filename),
@@ -32,7 +119,8 @@ public class LibraryPackagerTests
             assemblyBytes: [],
             debugSymbols: [],
             cSharpSourceCodeById: [],
-            elmLibrarySet: new LibrarySet("", [elmLibrary] ), resourceCanonicalBuilder: (_,_,_) => "");
+            elmLibrarySet: new LibrarySet("", [elmLibrary] ),
+            resourceCanonicalBuilder: (_, _, _) => "");
 
         // Assert
         Assert.IsNotNull(library);
