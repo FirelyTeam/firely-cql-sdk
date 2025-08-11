06640ff6eac4d856ba1e1c750cf57e3c3c7f2b4b
Paul den Boer <denboer.paul@gmail.com>
2025-08-11 14:52:36 +0200

    update public api

--- Cql/Cql.Invocation/Toolkit/CqlParameterInfo.cs
+++ Cql/Cql.Invocation/Toolkit/CqlParameterInfo.cs
@@ -9,5 +9,13 @@ namespace Hl7.Cql.Invocation.Toolkit;
 /// <param name="Type">The type of the parameter.</param>
 public readonly record struct CqlParameterInfo(string Name, Type Type)
 {
+    /// <summary>
+    /// Returns a string representation of the CQL parameter information, 
+    /// including its type and name.
+    /// </summary>
+    /// <returns>
+    /// A string in the format "<c>{Type}</c> '<c>{Name}</c>'", where <c>{Type}</c> 
+    /// is the C# representation of the parameter's type and <c>{Name}</c> is the parameter's name.
+    /// </returns>
     public override string ToString() => $"{Type.ToCSharpString()} '{Name}'";
 }
