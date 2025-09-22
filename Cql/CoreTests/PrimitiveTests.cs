59b6abbaae84ddee83869dc8723ea2edd73c59e9
Paul den Boer <denboer.paul@gmail.com>
2025-09-22 21:48:34 +0200

    Enable nullable annotations and add CqlQuantity tests
    Enabled `#nullable` annotations to improve null safety and reorganized `using` directives for clarity. Added a new `CqlQuantityTests` class with unit tests to validate `CqlQuantity` behavior, including negation operations and handling of `null` values. Made minor formatting adjustments for consistency and readability.

--- Cql/CoreTests/PrimitiveTests.cs
+++ Cql/CoreTests/PrimitiveTests.cs
@@ -6,18 +6,18 @@
  * available at https://raw.githubusercontent.com/FirelyTeam/firely-cql-sdk/main/LICENSE
  */
 
+#nullable enable
 using Hl7.Cql.CodeGeneration.NET.Toolkit;
 using Hl7.Cql.Compiler;
-using Hl7.Cql.Elm;
 using Hl7.Cql.Fhir;
 using Hl7.Cql.Iso8601;
 using Hl7.Cql.Operators;
 using Hl7.Cql.Primitives;
 using Hl7.Cql.Runtime;
-using Hl7.Cql.Runtime.Hosting;                                 
+using Hl7.Cql.Runtime.Hosting;
 
 namespace CoreTests
-{         
+{
     using DateTimePrecision = Hl7.Cql.Iso8601.DateTimePrecision;
     using Expression = System.Linq.Expressions.Expression;
 
@@ -165,7 +165,7 @@ namespace CoreTests
 
             var tdExpr = Expression.Constant(threeDays);
             var odExpr = Expression.Constant(oneDay);
-            
+
             var rc = GetNewContext();
             var fcq = rc.Operators;
             var memExpr = Expression.Constant(fcq);
@@ -275,6 +275,73 @@ namespace CoreTests
             boundariesBetween = new CqlDateTime(startDate).WholeCalendarPeriodsBetween(cqlStartDate, "month");
             Assert.AreEqual(16, boundariesBetween);
 
+        }
+    }
+
+    [TestClass]
+    [TestCategory("UnitTest")]
+    public class CqlQuantityTests
+    {
+        [TestMethod]
+        public void Negate_PositiveValue_ReturnsNegativeValue()
+        {
+            var quantity = new CqlQuantity(5.5m, "mg");
+            var negated = CqlQuantity.Negate(quantity);
+
+            Assert.IsNotNull(negated);
+            Assert.AreEqual(-5.5m, negated.value);
+            Assert.AreEqual("mg", negated.unit);
+        }
+
+        [TestMethod]
+        public void Negate_NullValue_ReturnsNullValueWithUnit()
+        {
+            var quantity = new CqlQuantity(null, "mg");
+            var negated = CqlQuantity.Negate(quantity);
+
+            Assert.IsNotNull(negated);
+            Assert.IsNull(negated.value);
+            Assert.AreEqual("mg", negated.unit);
+        }
+
+        [TestMethod]
+        public void Negate_NullQuantity_ReturnsNull()
+        {
+            CqlQuantity? quantity = null;
+            var negated = CqlQuantity.Negate(quantity);
+
+            Assert.IsNull(negated);
+        }
+
+        [TestMethod]
+        public void OperatorNegate_PositiveValue_ReturnsNegativeValue()
+        {
+            var quantity = new CqlQuantity(10m, "g");
+            var negated = -quantity;
+
+            Assert.IsNotNull(negated);
+            Assert.AreEqual(-10m, negated.value);
+            Assert.AreEqual("g", negated.unit);
+        }
+
+        [TestMethod]
+        public void OperatorNegate_NullValue_ReturnsNullValueWithUnit()
+        {
+            var quantity = new CqlQuantity(null, "g");
+            var negated = -quantity;
+
+            Assert.IsNotNull(negated);
+            Assert.IsNull(negated.value);
+            Assert.AreEqual("g", negated.unit);
+        }
+
+        [TestMethod]
+        public void OperatorNegate_NullQuantity_ReturnsNull()
+        {
+            CqlQuantity? quantity = null;
+            var negated = -quantity;
+
+            Assert.IsNull(negated);
         }
     }
 
